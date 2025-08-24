9153e622ad12c57d650df928a5b13ac29f33b3f0
Jo3n3n <goenen.jeremy@hotmail.de>
2025-08-24 18:14:36 +0200

    Update special_types_list.asm

--- data/battle/special_types_list.asm
+++ data/battle/special_types_list.asm
@@ -1,9 +1,9 @@
 ; marcelnote - new list to determine Physical / Special types
 SpecialTypesList:
-	db TRUE      ; $00 ; NORMAL from < > to TRUE
-	db TRUE      ; $01 ; FIGHTING from < > to TRUE
-	db TRUE      ; $02 ; FLYING from < > to TRUE
-	db TRUE      ; $03 ; POISON from < > to TRUE
+	db TRUE      ; $00 ; NORMAL to TRUE
+	db FALSE     ; $01 ; FIGHTING
+	db TRUE      ; $02 ; FLYING to TRUE
+	db FALSE     ; $03 ; POISON
 	db FALSE     ; $04 ; GROUND
 	db FALSE     ; $05 ; ROCK
 	db FALSE     ; $06 ; BIRD
@@ -21,10 +21,10 @@ SpecialTypesList:
 	db FALSE     ; $12 ; unused type
 	db FALSE     ; $13 ; unused type
 	db TRUE      ; $14 ; FIRE
-	db FALSE     ; $15 ; WATER from < > to FALSE
-	db FALSE     ; $16 ; GRASS from < > to FALSE
+	db FALSE     ; $15 ; WATER to FALSE
+	db FALSE     ; $16 ; GRASS to FALSE
 	db TRUE      ; $17 ; ELECTRIC
 	db TRUE      ; $18 ; PSYCHIC_TYPE
-	db FALSE     ; $19 ; ICE from < > to FALSE
+	db FALSE     ; $19 ; ICE to FALSE
 	db FALSE     ; $1A ; DRAGON
 	db -1 ; end
