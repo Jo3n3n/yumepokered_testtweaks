7020aa806430740c3114bc55b2ed89e2c3a85fd5
Jo3n3n <goenen.jeremy@hotmail.de>
2025-08-27 08:34:22 +0200

    Update VictoryRoad3F.asm
    buffed Levels

--- data/wild/maps/VictoryRoad3F.asm
+++ data/wild/maps/VictoryRoad3F.asm
@@ -1,15 +1,15 @@
 VictoryRoad3FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 30, MACHOP   ; marcelnote - was 24, MACHOP
-	db 32, GEODUDE  ; marcelnote - was 26, GEODUDE
-	db 39, GOLBAT   ; marcelnote - was 22, ZUBAT
-	db 42, ONIX
-	db 40, VENOMOTH
-	db 45, ONIX
-	db 43, GRAVELER
-	db 41, GOLBAT
-	db 42, MACHOKE
-	db 45, MACHOKE
+	db 45, MACHOP   ; marcelnote - was 24, MACHOP
+	db 44, GEODUDE  ; marcelnote - was 26, GEODUDE
+	db 46, GOLBAT   ; marcelnote - was 22, ZUBAT
+	db 51, ONIX
+	db 49, VENOMOTH
+	db 47, ONIX
+	db 50, GRAVELER
+	db 48, GOLBAT
+	db 48, MACHOKE
+	db 52, MACHOKE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
